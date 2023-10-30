4f56f7adb7addfc0ca8bdb2610e463bf75e09a86
dahniardd <dahniardvn09@gmail.com>
2023-10-30 11:52:04 +0700

    Modifikasi Percobaan 3

--- ArrayNilaiRata06.java
+++ ArrayNilaiRata06.java
@@ -2,22 +2,35 @@ import java.util.Scanner;
 public class ArrayNilaiRata06 {
     public static void main (String[]Args){
         try (Scanner input06 = new Scanner(System.in)){
-            int[] nilaiMhs = new int[10];
-            double total=0;
-            double rata2;
-            
-            for (int i=0; i < nilaiMhs.length; i++){
-                System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+":");
+            System.out.print("Masukkan jumlah mahasiswa : ");
+            int jumlahMhs = input06.nextInt();
+            int[] nilaiMhs = new int[jumlahMhs];
+            double totalLulus = 0;
+            double totalTidakLulus = 0;
+            int jmlLulus = 0;
+            int jmlTdkLulus = 0;
 
+            for (int i = 0; i < nilaiMhs.length; i++){
+                System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+                
                 nilaiMhs[i] = input06.nextInt();
-            }
-            for (int i=0; i < nilaiMhs.length; i++){
-                total += nilaiMhs[i];
 
-            }
-            rata2 = total/nilaiMhs.length;
-            System.out.println("Rata Rata Nilai = "+rata2);
-            }
-        } 
-    }
-
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                jmlLulus++;
+            } 
+            
+            else {
+               totalTidakLulus += nilaiMhs[i];
+               jmlTdkLulus++;
+            } 
+        }
+            double rataLulus = (jmlLulus>0) ? totalLulus/jmlLulus : 0;
+            double rataTdkLulus = (jmlTdkLulus > 0) ?
+            totalTidakLulus /jmlTdkLulus : 0;
+            System.out.println("Rata-rata nilai lulus = "
+            +rataLulus);
+            System.out.println("Rata-rata nilai tidak lulus = " +rataTdkLulus);
+}
+}
+}
