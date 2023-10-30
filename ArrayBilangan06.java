4bac2bedba31af0bae62995ea5639eb3530416f6
dahniardd <dahniardvn09@gmail.com>
2023-10-30 09:52:41 +0700

    Modifikasi Percobaan 1

--- ArrayBilangan06.java
+++ ArrayBilangan06.java
@@ -3,15 +3,11 @@ public class ArrayBilangan06{
     public static void main(String [] Args){
         Scanner input06 = new Scanner(System.in);
 
-        int [] bil = new int [4];
-        bil[0]=5;
-        bil[1]=13;
-        bil[2]=-7;
-        bil[3]=17;
+        int [] bil = {12867, 5, 7, 2000000};
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i <= 4; i++){
+        System.out.println(bil[i]);
+        }
+
     }
 }
